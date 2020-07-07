d994de976c27de5f9cdb43647c1561883844921e
Ethar94 <Ethar.jehad@gmail.com>
2020-07-07 20:01:56 +0300

    login component done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-
+import LoginT from './LoginT.js'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -37,7 +36,9 @@ export default  function App() {
           <Button color="inherit" herf="">Teacher</Button>
           <Button color="inherit" herf="/Students">Student</Button>
         </Toolbar>
-      </AppBar>
+      </AppBar>    
+
+      <LoginT/>
     </div>
   );
 }
