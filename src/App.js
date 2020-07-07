a9e87c2f84d90efd89112a821dee454f58d461bc
rwabusini <rwabusini@gmail.com>
2020-07-07 20:15:05 +0300

    adding session

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -19,6 +20,9 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
 }));
+// const teacher = (props) => {
+//   props.history.push({ pathname: '/LoginT' });
+// }
 
 
 export default  function App() {
@@ -34,7 +38,8 @@ export default  function App() {
           <Typography variant="h6" className={classes.title}>
             Book Teachers
           </Typography>
-          <Button color="inherit" herf="">Teacher</Button>
+          <Button color="inherit" >Teacher</Button>
+          
           <Button color="inherit" herf="/Students">Student</Button>
         </Toolbar>
       </AppBar>
