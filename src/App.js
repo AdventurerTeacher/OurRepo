cf7e1654a2209e648346a71aa91181adb17a1090
Baraa-Diab <baraalutfi10@gmail.com>
2020-07-08 17:39:58 +0300

    Font end -singupT

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import SignupT from './SignupT';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,26 +20,35 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
-export default  function App() {
+export default function App() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
             {/* <MenuIcon /> */}
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             Book Teachers
           </Typography>
-          <Button color="inherit" herf="">Teacher</Button>
-          <Button color="inherit" herf="/Students">Student</Button>
+          <Button color="inherit" herf="">
+            Teacher
+          </Button>
+          <Button color="inherit" herf="/Students">
+            Student
+          </Button>
         </Toolbar>
       </AppBar>
+      <SignupT/>
     </div>
   );
 }
 
-// export default App;
+//export default App;
--- src/SignupT.js
+++ src/SignupT.js
@@ -3,8 +3,8 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
+//import FormControlLabel from "@material-ui/core/FormControlLabel";
+//import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
@@ -16,9 +16,9 @@ import Container from "@material-ui/core/Container";
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
+      {"Welcome to our Website  "}
       <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+        Teachers
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
@@ -141,12 +141,6 @@ export default function SignUp() {
                 autoComplete="current-password"
               />
             </Grid>
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
-              />
-            </Grid>
           </Grid>
           <Button
             type="submit"
@@ -174,3 +168,4 @@ export default function SignUp() {
 }
 
 // export default App;
+//export default singupT;
